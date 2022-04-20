d8224397a64704adbf82adaf9fb6df3dd981748f
snipperrjay <alfredabaidoo7@gmail.com>
2022-04-20 12:48:11 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -10,19 +10,7 @@
 
 void reset_to_98(int *n);
 {
-	int n;
-	int *p;
-
-	n = 98;
-	p = &n;
-	_putchar("Value of 'n' before call: %d\n", n);
-	_putchar("value of 'p': %p\n", p);
-	_putchar("Address of 'n': %p\n", n);
-	_putchar("Address of 'p': %p\n", p);
-	
-	modif_my_param(p);
-	_putchar("Value of 'n' fter the call: %d\n", n);
-
+	*n = 98;
 
 	return 0;
 }
