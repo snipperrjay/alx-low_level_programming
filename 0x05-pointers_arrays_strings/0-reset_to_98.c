44d4f8f3206d6fa1f2118315470dd3155f108cd0
snipperrjay <alfredabaidoo7@gmail.com>
2022-04-20 12:22:15 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *
+ * modif_my_param set the integer to 98
+ * Return: Always 0
+ **/
+
+
+int main(void)
+{
+	int n;
+	int *p;
+
+	n = 98;
+	p = &n;
+	_putchar("Value of 'n' before call: %d\n", n);
+	_putchar("value of 'p': %p\n", p);
+	_putchar("Address of 'n': %p\n", n);
+	_putchar("Address of 'p': %p\n", p);
+	
+	modif_my_param(p);
+	_putchar("Value of 'n' fter the call: %d\n", n);
+
+
+	return 0;
+}
+
+ 
+
