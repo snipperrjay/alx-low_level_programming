26fa9181221bb979960e39bb45eb33b980be09e4
snipperrjay <alfredabaidoo7@gmail.com>
2022-04-20 12:52:20 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * int _putchar(char c)
- *
- * modif_my_param set the integer to 98
- * Return: Always 0
+ * reset_to_98 - update address function
+ *@n: integer
+ * 
+ * Return: nothing 
  **/
 
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
-
-	return 0;
 }
