793ad0dc4c40684766829658049737efedd71411
snipperrjay <alfredabaidoo7@gmail.com>
2022-05-09 15:40:56 -0700

    done

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,37 +2,27 @@
 
 /**
  * main - multiplies two positive numbers
- * 
- *_putchar
- *
  * @argc: n arguments
- *
  * @argv: args
- * 
  * Return: int
- *
  */
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int i, j;
+unsigned long mul;
+int i, j;
 	if (argc != 3)
-			{ 
-				_putchar("Error\n");
-				exit(98);
-		       	}
+	{ printf("Error\n");
+	exit(98); }
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  _putchar("Error\n");
-				exit(98);
-		       	}
+			{  printf("Error\n");
+			exit(98); }
 		}
-
 	}
-	mul = atol(argv[1]) * atol(argv[2]);
-	_putchar("%lu\n", mul);
-	return (0);
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
 }
